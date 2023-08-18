5ccbb4d15c10a55b7efed07adfaaa34c2bb4cae0
oscar50 <oscar@whaleandjaguar.co>
2023-08-18 07:22:26 -0500

    feat: include ocr

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
+import json
+import base64
 import pdfplumber
 import pandas as pd
+from PIL import Image
 import streamlit as st
 import plotly.graph_objects as go
 
@@ -8,18 +12,23 @@ st.set_page_config(page_title="Data Health Care",
                    layout="wide")
 
 
+def load_image(image_f):
+    img_f = Image.open(image_f)
+    return img_f
+
+
 st.markdown("### **Paciente:** Juan Perez, **Documento:** 100000002")
 st.markdown("#### Sin alarmas vigentes 👨🏽‍⚕️")
 st.markdown("* * *")
 
 with st.sidebar:
     st.markdown("# Bienvenidos a DHC 🏥")
-    st.markdown("## Paciente")
+    st.markdown("## Medico tratante")
     st.image("img/User-Profile-PNG.png")
-    st.markdown("**Nombre:** Juan Perez")
-    st.markdown("**Edad:** 49 años")
+    st.markdown("**Nombre:** Dr. Julio Flores")
+    st.markdown("**Especialidad:** Gastroenterologo")
     st.markdown("**Documento:** 100000002")
-    st.markdown("**Med. Tratante:** Dr. Julio Flores")
+
     st.warning('No olvidar verificar los datos del paciente', icon="⚠️")
 
 option = st.selectbox(
@@ -35,8 +44,38 @@ if option == "pdf":
             first_page = pdf.pages[0]
             st.write(first_page.chars[0])
 else:
-    uploaded_file = st.file_uploader("Sube una imagen 'png', 'jpeg', 'jpg'", type=['png', 'jpeg', 'jpg'])
-    if uploaded_file is not None:
-        st.image(uploaded_file)
+    image_file = st.file_uploader("Sube una imagen 'png', 'jpeg', 'jpg'", type=['png', 'jpeg', 'jpg'])
+    if image_file is not None:
+        file_details = {"FileName": image_file.name, "FileType": image_file.type}
+        st.write(file_details)
+        img = load_image(image_file)
+        st.image(img)
+        name = os.path.join("temp_data", image_file.name)
+        with open(name, "wb") as f:
+            f.write(image_file.getbuffer())
+        st.success("Saved File")
+
+        if st.button("Procesar"):
+            with open(name, 'rb') as f:
+                image = f.read()
+
+            encoded_bytes = base64.b64encode(image)
+            encoded_string = encoded_bytes.decode('utf-8')
+
+            with open('temp_data/python-encoded.txt', 'w') as f:
+                f.write(encoded_string)
+
+            st.markdown("* * *")
+            st.markdown("## Resumen del documento")
+
+            with open('data.json') as file:
+                data = json.load(file)
+
+                st.write(data)
+
+                st.write(data.get("paciente").get("nombre"))
+                st.write(data.get("paciente").get("edad"))
+                st.write(data.get("paciente").get("sexo"))
+
 
 st.markdown("* * *")
